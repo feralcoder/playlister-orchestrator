a3905e0a2828ad23b0168fa4bd1fff59407e32f4
feralcoder <feralcoder@gmail.com>
2021-04-20 06:55:12 +0000

    gathering VIP and frontend/backend IPs of all instances

--- 03_manage_heatstack.sh
+++ 03_manage_heatstack.sh
@@ -7,7 +7,6 @@ TEMPLATE_DIR=$PLAYLISTER_DIR/playlister.heat/
 [ "${BASH_SOURCE[0]}" -ef "$0" ]  || { echo "Don't source this script!  Run it."; return 1; }
 
 source_host_control_scripts       || fail_exit "source_host_control_scripts"
-new_venv playlister               || fail_exit "new_venv kolla-ansible"
 use_venv playlister               || fail_exit "use_venv kolla-ansible"
 . /etc/kolla/admin-openrc.sh
 
@@ -31,6 +30,10 @@ BE_STACK_IDS=`for NAME in $BE_STACK_NAMES; do
   echo "$STACK_LIST" | grep $NAME | awk '{print $2}'
 done`
 
+FE_LB_STACK_NAME=`echo "$RESOURCE_LIST" | grep " fe_loadbalancer " | awk '{print $12}'`
+FE_LB_STACK_ID=`echo "$STACK_LIST" | grep " $FE_LB_STACK_NAME " | awk '{print $2}'`
+FE_VIP=`openstack stack output show $FE_LB_STACK_ID vip_ip -f json | jq '.["output_value"]' | sed 's/"//g'`
+
 FE_IPS=`for ID in $FE_STACK_IDS; do
   openstack stack output show $ID server_ip -f json | jq '.["output_value"]' | sed 's/"//g'
 done`
@@ -38,3 +41,7 @@ done`
 BE_IPS=`for ID in $BE_STACK_IDS; do
   openstack stack output show $ID server_ip -f json | jq '.["output_value"]' | sed 's/"//g'
 done`
+
+echo FE_VIP: $FE_VIP
+echo FE_IPS: $FE_IPS
+echo BE_IPS: $BE_IPS
