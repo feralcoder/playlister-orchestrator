e60d7b221c64db89a3a088b75ec7b0ab1c40c029
feralcoder <feralcoder@gmail.com>
2021-04-21 19:20:01 +0000

    Prompt for playlister user password once, mark up openrc.sh

--- 01a_setup_playlister_identity.sh
+++ 01a_setup_playlister_identity.sh
@@ -10,11 +10,16 @@ KEYNAME=cliff_admin
 
 
 
+echo "ENTER PLAYLISTER USER PASSWORD:"
+read -sr OS_PASSWORD_INPUT
+
+sed "s/__PASSWORD__/$OS_PASSWORD_INPUT/g" $FERALSTACK_SETUP_DIR/playlister-openrc.sh.template > $FERALSTACK_SETUP_DIR/playlister-openrc.sh
+
 openstack project create --domain default --description "playlister" playlister
 
 
-#openstack user create --domain default --password $PASS playlister
-openstack user create --domain default --password-prompt playlister
+openstack user create --domain default --password $OS_PASSWORD_INPUT playlister
+#openstack user create --domain default --password-prompt playlister
 
 # heat_stack_user  role actually breaks ability to see heat stacks...
 for ROLE in load-balancer_global_observer  load-balancer_observer heat_stack_owner load-balancer_admin _member_  load-balancer_member load-balancer_quota_admin reader member admin ; do 
