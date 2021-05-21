1c401c482a7a9a75f672349d063a79dd97d7d63f
feralcoder <feralcoder@gmail.com>
2021-05-21 00:13:02 +0000

    startup handled by orchestrator state machine, don't enable cassandra service

--- playlister-python/playlister_settings.py
+++ playlister-python/playlister_settings.py
@@ -1,2 +1,6 @@
 playlister_settings = {}
+playlister_settings['playlister'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
 playlister_settings['playlister2'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
+playlister_settings['playlister3'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
+playlister_settings['playlister4'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
+playlister_settings['playlister5'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
