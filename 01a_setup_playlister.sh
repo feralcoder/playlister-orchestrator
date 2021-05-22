a4135c96cfb7fc401cd218ad4b59b51c00455f05
feralcoder <feralcoder@gmail.com>
2021-05-22 20:22:28 +0000

    add key setup playlister:cliff_admin

--- 01a_setup_playlister.sh
+++ 01a_setup_playlister.sh
@@ -21,6 +21,15 @@ chmod 700 $PLAYLISTER_SETUP_DIR/playlister-openrc.sh
 KEYPATH=~/.ssh/keypair.cliff_admin.private
 KEYNAME=cliff_admin
 
+key_setup () {
+  # Encrypted via: openssl enc -aes-256-cfb8 -md sha256 -in $KOLLA_SETUP_DIR/../files/keypair.cliff_admin.private -out $KOLLA_SETUP_DIR/../files/keypair.cliff_admin.encrypted
+#  openssl enc --pass file:/home/cliff/.password -d -aes-256-cfb8 -md sha256 -in $KOLLA_SETUP_DIR/../files/keypair.cliff_admin.encrypted -out $KOLLA_SETUP_DIR/../files/keypair.cliff_admin.private
+  openssl enc --pass file:/home/cliff/.password -d -aes-256-cfb8 -md sha256 -in $PLAYLISTER_SETUP_DIR/files/keypair.cliff_admin.encrypted -out $PLAYLISTER_SETUP_DIR/files/keypair.cliff_admin.private
+  chmod 600 $PLAYLISTER_SETUP_DIR/files/keypair.cliff_admin.private
+  mv $PLAYLISTER_SETUP_DIR/files/keypair.cliff_admin.private ~/.ssh/
+  openstack keypair create cliff_admin --public-key $PLAYLISTER_SETUP_DIR/files/keypair.cliff_admin.public
+}
+
 
 # Assume if file doesn't exist, we're not on kolla-ansible controller and identity portion has already been run...
 [[ -d $KOLLA_SETUP_DIR ]] && {
@@ -33,7 +42,7 @@ KEYNAME=cliff_admin
   openstack keypair create cliff_admin --public-key $KOLLA_SETUP_DIR/../files/keypair.cliff_admin.public
 
   openstack project create --domain default --description "playlister" playlister
-  #openstack user create --domain default --password-prompt playlister
+  openstack user create --domain default --password `cat $PLAYLISTER_SETUP_DIR/playlister-openrc.sh | grep '^export OS_PASSWORD' | awk -F'=' '{print $2}'`  playlister
 
   # heat_stack_user  role actually breaks ability to see heat stacks...
   for ROLE in load-balancer_global_observer  load-balancer_observer heat_stack_owner load-balancer_admin _member_  load-balancer_member load-balancer_quota_admin reader member admin ; do 
@@ -44,3 +53,4 @@ KEYNAME=cliff_admin
 
 use_venv playlister
 . $PLAYLISTER_SETUP_DIR/playlister-openrc.sh
+key_setup
