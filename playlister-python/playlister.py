1d930972971e66ca6c8b324306c2dc85419054c0
feralcoder <feralcoder@gmail.com>
2021-06-13 06:26:10 +0000

    tweaks for orchestrator

--- playlister-python/playlister.py
+++ playlister-python/playlister.py
@@ -1,25 +1,44 @@
 import os
 import sys
 from orchestrator import HostCommander, NodeManager, PlaylisterDeployer, PuppetManifest
+import logging, sys
 
 stack_name = sys.argv[1]
 script_dir=os.path.dirname(os.path.realpath(__file__))
 manifest_dir="{0}/puppet-manifests".format(script_dir)
 puppetmaster = 'puppetmaster.feralcoder.org'
 
+log_level=logging.INFO
+logging.basicConfig(stream=sys.stderr, level=log_level)
 
 if __name__ == "__main__":
-  manifest = PuppetManifest(manifest_dir, stack_name, "production", puppetmaster)
+  manifest = PuppetManifest(manifest_dir, stack_name, puppetmaster, "production")
+  logging.debug(repr(manifest))
   manifest.git_pull()
   playlister_deployer = PlaylisterDeployer(stack_name, manifest, script_dir)
+  logging.debug(repr(playlister_deployer))
 
-  # NEW MARIADB WAY
-  #playlister_deployer.deploy_this_thing()
-  #playlister_deployer.start_galera()
+  # DEPLOY PLAYLISTER
+  # create_cluster done for OLAP / OLTP services...
+  #playlister_deployer.create_cluster()
+  # IMPLEMENT:
   #playlister_deployer.load_schema()
-  #playlister_deployer.start_columnnstore()
-  #playlister_deployer.shutdown_galera()
-  #playlister_deployer.shutdown_columnstore()
+
+  # START PLAYLISTER
+  #playlister_deployer.start_transaction_db()
+  # IMPLEMENT:
+  #playlister_deployer.start_analytics_db()
+
+  # STOP PLAYLISTER
+  #playlister_deployer.stop_transaction_db()
+  # IMPLEMENT:
+  #playlister_deployer.stop_analytics_db()
+
+  # DEPLOY PLAYLISTER
+  playlister_deployer.deploy_playlister_code(parallel=True)
+
+
+
 
   # OLD CASSANDRA WAY
   #playlister_deployer.deploy_this_thing()
--- playlister-python/playlister_settings.py
+++ playlister-python/playlister_settings.py
@@ -1,6 +1,6 @@
 playlister_settings = {}
-playlister_settings['playlister'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
-playlister_settings['playlister2'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
-playlister_settings['playlister3'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
-playlister_settings['playlister4'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
-playlister_settings['playlister5'] = {'cluster_name': 'Playlister Test Cluster', 'DC': 'DC1', 'rack': 'Rack1', 'package_ensure': '3.0.5-1', 'package': 'cassandra30'}
+playlister_settings['playlister'] = {'cluster_name': 'playlister'}
+playlister_settings['playlister2'] = {'cluster_name': 'playlister2'}
+playlister_settings['playlister3'] = {'cluster_name': 'playlister3'}
+playlister_settings['playlister4'] = {'cluster_name': 'playlister4'}
+playlister_settings['playlister5'] = {'cluster_name': 'playlister5'}
