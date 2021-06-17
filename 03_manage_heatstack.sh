fd45e7ef788787d98b50ce392d498251aa203b4b
feralcoder <feralcoder@gmail.com>
2021-06-17 06:06:55 +0000

    cleanup

--- 03_manage_heatstack.sh
+++ 03_manage_heatstack.sh
@@ -18,15 +18,15 @@ STACK_NAME=$1
 
 rename_servers () {
   SERVER_LINES=`openstack server list --all | grep -v '\-\-\-\| Name ' | grep " ${STACK_NAME}[_-]"`
-  FE_LINES=`echo "$SERVER_LINES" | grep " ${STACK_NAME}_fe[ -]"`
-  OLAP_LINES=`echo "$SERVER_LINES" | grep " ${STACK_NAME}_olap[ -]"`
-  OLTP_LINES=`echo "$SERVER_LINES" | grep " ${STACK_NAME}_oltp[ -]"`
+  FE_LINES=`echo "$SERVER_LINES" | grep " ${STACK_NAME}[_-]fe[ -]"`
+  OLAP_LINES=`echo "$SERVER_LINES" | grep " ${STACK_NAME}[_-]olap[ -]"`
+  OLTP_LINES=`echo "$SERVER_LINES" | grep " ${STACK_NAME}[_-]oltp[ -]"`
 
   ID_IP_PAIRS=$(while read -r LINE; do
     ID=`echo $LINE | awk '{print $2}'`
     IP=`echo $LINE | awk '{print $8}' | awk -F'=' '{print $2}'`
     echo $ID:$IP
-  done < <(printf '%s\n' "$SERVER_LINES"))
+#  done < <(printf '%s\n' "$FE_LINES\n$OLAP_LINES\n$OLTP_LINES"))
 
   for PAIR in $ID_IP_PAIRS; do
     ID=`echo $PAIR | awk -F':' '{print $1}'`
@@ -44,41 +44,10 @@ rename_servers () {
     echo "Resetting $HOSTNAME.novalocal on puppetmaster"
     ssh root@puppetmaster "/opt/puppetlabs/bin/puppetserver ca clean --certname $HOSTNAME.novalocal"
     # PUPPET PROBABLY RAN AND BJORKED ITSELF...
-    ssh -J admin1 -o "StrictHostKeyChecking no" -i ~/.ssh/keypair.cliff_admin.private -l centos $IP "sudo puppet resource service puppet ensure=stopped"
-    ssh -J admin1 -o "StrictHostKeyChecking no" -i ~/.ssh/keypair.cliff_admin.private -l centos $IP "sudo rm -rf /etc/puppetlabs/puppet/ssl/*"
-    ssh -J admin1 -o "StrictHostKeyChecking no" -i ~/.ssh/keypair.cliff_admin.private -l centos $IP "sudo puppet agent --test"
+    ssh -J admin1 -o "StrictHostKeyChecking no" -i ~/.ssh/keypair.cliff_admin.private -l centos $IP "sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=stopped"
+    ssh -J admin1 -o "StrictHostKeyChecking no" -i ~/.ssh/keypair.cliff_admin.private -l centos $IP "sudo /usr/bin/rm -rf /etc/puppetlabs/puppet/ssl/"
+    ssh -J admin1 -o "StrictHostKeyChecking no" -i ~/.ssh/keypair.cliff_admin.private -l centos $IP "sudo /opt/puppetlabs/bin/puppet agent --test"
   done
 }
-#
-#gather_details () {
-#  STACK_ID=$(openstack stack list | grep " $STACK_NAME " | awk '{print $2}')
-#  STACK_LIST=$(openstack stack list --nested | grep $STACK_NAME) || return 1
-#  RESOURCE_LIST=$(openstack stack resource list $STACK_ID --nested-depth 10) || return 1
-#  OLAP_STACK_NAMES=$(echo "$RESOURCE_LIST" | grep " olap_backend " | awk '{print $12}')
-#  OLTP_STACK_NAMES=$(echo "$RESOURCE_LIST" | grep " oltp_backend " | awk '{print $12}')
-#  OLAP_STACK_IDS=$(for NAME in $OLAP_STACK_NAMES; do
-#    echo "$STACK_LIST" | grep $NAME | awk '{print $2}'
-#  done)
-#  OLTP_STACK_IDS=$(for NAME in $OLTP_STACK_NAMES; do
-#    echo "$STACK_LIST" | grep $NAME | awk '{print $2}'
-#  done)
-#  OLAP_IPS=$(for ID in $OLAP_STACK_IDS; do
-#    openstack stack output show $ID server_ip -f json | jq '.["output_value"]' | sed 's/"//g' || return 1
-#  done)
-#  OLTP_IPS=$(for ID in $OLTP_STACK_IDS; do
-#    openstack stack output show $ID server_ip -f json | jq '.["output_value"]' | sed 's/"//g' || return 1
-#  done)
-#}
-#
-#print_details () {
-#  echo "OLAP_STACK_NAMES:" $OLAP_STACK_NAMES
-#  echo "OLTP_STACK_NAMES:" $OLTP_STACK_NAMES
-#  echo "OLAP_STACK_IDS:" $OLAP_STACK_IDS
-#  echo "OLTP_STACK_IDS:" $OLTP_STACK_IDS
-#  echo "OLAP_STACK_IPS:" $OLAP_STACK_IPS
-#  echo "OLTP_STACK_IPS:" $OLTP_STACK_IPS
-#}
 
 rename_servers
-#gather_details
-#print_details
